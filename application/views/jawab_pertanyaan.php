f7c88fb889443cc05c79675683a02967fab17fbb
indanazf <izulfa28@gmail.com>
2013-03-24 20:20:18 +0700

    jawab pertanyaan

---
+++ application/views/jawab_pertanyaan.php
@@ -0,0 +1,24 @@
+		<div data-role="content"> 
+			<p class="text"><i>soal</i></p> 
+			<div data-role="fieldcontain" class="ui-hide-label">
+				<div class="choice_list">		
+				<ul data-role="listview" data-inset="true" data-theme="d"> <!---menampilkan pertanyaan-->
+					<li>
+					
+					</li>				
+				</ul>
+				</div>			
+			</div>
+			<div data-role="fieldcontain" class="ui-hide-label">
+				<input type="text" name="" id="" placeholder="jawaban"/>
+				<button type="submit" data-inline="true">jawab</button>
+			</div>
+			
+			 <div style="position: absolute; top: 50%; margin-top: -1.2em;">
+				<a href="" data-role="button">acak</a>
+			</div>
+		</div> 
+
+		<div id="user-info"></div>
+		
+	
--- application/views/profile.php
+++ application/views/profile.php
@@ -18,9 +18,12 @@
 					soal
 				</div> 
 			</div> 
+			<div data-role = "fieldcontain" class="ui-hide-label">
+				<a href="<?php echo base_url()?>index.php/"><img src="<?php echo base_url()?>css/images/celengan.png" width="25px" height="25px"></a>
+				<a href="<?php echo base_url()?>index.php/"><img src="<?php echo base_url()?>css/images/penghargaan.png" width="25px" height="25px"></a>
+			</div>
 			
-			
-			
+			<p class="text">----------<i>penjawab</i>----------</p>
 		</div> 
 
 		<div id="user-info"></div>
