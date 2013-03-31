8ae195281a697eca1e5d556cb93b78694a4a299f
ratihpujihati <ratih.pujihati@gmail.com>
2013-03-31 14:55:05 +0700

    edit lagi ubah profil

--- application/views/profile_ubah_view.php
+++ application/views/profile_ubah_view.php
@@ -2,7 +2,7 @@
 <div data-role="content"> 
 	<p class="text"><i><?php echo $row->username; ?></i></p> <!-- untuk menampilkan nama user -->
 	<div class="ui-body ui-body-d">
-		<form method="POST" action="<?php echo base_url()?>index.php/soal/simpan_profile_ubah" data-ajax="false"  data-transition="pop">
+		<form method="POST" action="<?php echo base_url()?>index.php/Profil/simpan_profile_ubah" data-ajax="false"  data-transition="pop">
 			<input type="hidden" name="username" value="<?php echo $row->username;?>"/> 		
 					
 				<textarea type="text" name="bio" id="bio"><?php echo $row->bio;?></textarea>
