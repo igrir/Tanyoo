11fb7e7a228e1a3383c896d483b3f4455c272d8e
indanazf <izulfa28@gmail.com>
2013-04-02 03:48:37 +0700

    tampilan kasar untuk statistik

--- application/views/statistik.php
+++ application/views/statistik.php
@@ -3,10 +3,29 @@
 		<p class="text">-------<i>statistik</i>------</p>
 		<p class="text"></p> <!--- tampilkan nama user -->
 
-		<div data-role="fieldcontain" class="ui-hide-label">
-			
+		<ul data-role="listview" data-inset="true" data-theme="e"> 
+			<li>
+				<p>Banyak Soal : </p>
+				<p>Banyak Pengguna : </p>
+				<p>Peserta : </p>
+			</li>						
+		</ul>
 		
-		</div>
+		<ul data-role="listview" data-inset="true" data-theme="d"> <!---menampilkan 3 nama peserta skor tertinggi-->
+			<li data-role="list-divider">Rangking 1</li>
+			<li>
+				<a href=""><p></p></a> <!--masukkan nama-->
+			</li>
+			<li data-role="list-divider">Rangking 2</li>
+			<li>
+				<a href=""><p></p></a> <!--masukkan nama-->
+			</li>
+			<li data-role="list-divider">Rangking 3</li>
+			<li>
+				<a href=""><p></p></a> <!--masukkan nama-->
+			</li>
+		</ul>
+		
 	</div>
 	<div id="user-info"></div>
 
