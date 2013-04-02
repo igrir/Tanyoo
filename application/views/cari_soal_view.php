212a03608cbcdbb0511295ebf019d506cc761de4
ratihpujihati <ratih.pujihati@gmail.com>
2013-04-03 04:14:14 +0700

    perbaiki pencarian soal

--- application/views/cari_soal_view.php
+++ application/views/cari_soal_view.php
@@ -4,7 +4,7 @@
 		<form action="<?php echo base_url()?>index.php/soal/proses_cari_soal" method="POST" data-ajax="false">
 			<table>
 				<tr>
-					<td><input type="text" placeholder="Pencarian" name="cari" size="60px" ></td>
+					<td><input type="text" placeholder="Pencarian" name="cari" size="60px" required></td>
 				</tr>
 				<tr>
 					<td><input type="Submit" value="Cari" name="bcari" data-ajax="false" data-inline="true"/></td>
@@ -13,17 +13,22 @@
 		</form>	
 		<?php 
 			if(isset($_POST['bcari'])){	?>
-				<?php $i=1 ?>
-				<?php foreach($data_soal as $t): //menampilkan hasil dari data_soal yang ada dicontroller ke dalam tabel?> 
-				<div class="choice_list">		
-				<ul data-role="listview" data-inset="true" data-theme="d" id="tag">
-					<li><h3><?php echo $t->text_soal ?> ?</h3>
-					<p><i>#<?php echo $t->tag ?></i></p>
-					<p><?php echo anchor('soal/jawab_id/'.$t->id_soal,'jawab');?></p>
-				</ul>
-				<?php $i++; ?>
-				<?php endforeach ?>						
-		<?php
+				<?php $i=1 ;?>
+				<?php if (count($data_soal) > 0) {
+					foreach($data_soal as $t): //menampilkan hasil dari data_soal yang ada dicontroller ke dalam tabel?>   
+					<div class="choice_list">		
+					<ul data-role="listview" data-inset="true" data-theme="d" id="tag">
+						
+						<li><h3><?php echo $t->text_soal ?> ?</h3>
+						<p><i>#<?php echo $t->tag ?></i></p>
+						<p><?php echo anchor('soal/jawab_id/'.$t->id_soal,'jawab');?></p>
+					</ul>
+					<?php $i++;	 ?>
+					<?php endforeach ?>						
+				<?php	
+				}else {
+					echo "<div class='border'><h4 align='center'>Hasil pencarian tidak ditemukan.</h4></div>";
+				}
 			}
 		?>
 			
